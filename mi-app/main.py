c20cc3f09a451b0d5158c0309821bf6811a6d3a6
Juan Antonio Devesa Thomson <juanadevesat93@gmail.com>
2023-11-13 14:06:04 +0100

    def4

--- mi-app/main.py
+++ mi-app/main.py
@@ -6,7 +6,8 @@ def pruebacloudbuild(event, context):
          event (dict): Event payload.
          context (google.cloud.functions.Context): Metadata for the event.
     """
-    file = event
+    file = open(event)
     data = json.load(file)
     print(f"Processing file: {file['name']}.")
-    print(data)
+    print(data['emp_details'])
+    file.close()
